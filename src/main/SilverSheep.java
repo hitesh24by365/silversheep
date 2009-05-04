b4d2dd4eee932398285095c9043204462e4ad477
castidiablo <castidiablo@bbb5350a-2a9f-11de-979f-8d47a8973ba4>
2009-05-04 20:09:34 +0000

    He aniadido todas las clases e interfaces que hay en el diagrama de clases
    git-svn-id: http://silversheep.googlecode.com/svn/trunk@6 bbb5350a-2a9f-11de-979f-8d47a8973ba4

--- src/main/SilverSheep.java
+++ src/main/SilverSheep.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
+
 package main;
 
-/**
- * @author arch
- *
- */
 public class SilverSheep {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		new Tester();
 	}
