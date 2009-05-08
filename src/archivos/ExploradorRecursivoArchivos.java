b74d2adc94fabe9397bb78eea8a01319d4f6c4e5
castidiablo <castidiablo@bbb5350a-2a9f-11de-979f-8d47a8973ba4>
2009-05-08 14:09:31 +0000

    desde mi nuevo Gentoo
    git-svn-id: http://silversheep.googlecode.com/svn/trunk@8 bbb5350a-2a9f-11de-979f-8d47a8973ba4

--- src/archivos/ExploradorRecursivoArchivos.java
+++ src/archivos/ExploradorRecursivoArchivos.java
@@ -1,9 +1,23 @@
 package archivos;
 import java.io.File;
 import java.util.StringTokenizer;
+
+import main.ArchivoMultimedia;
+import main.Audio;
+import main.Video;
+import almacenamiento.Biblioteca;
+import almacenamiento.AlmacenarInfoBibliotecaRefinado;
+import almacenamiento.TransaccionesSQLite;
 public class ExploradorRecursivoArchivos {
-	private String[] extensiones; 
+	private String[] extensiones;
+	private Biblioteca biblio;
+	private TransaccionesSQLite sqlite;
+	private ArchivoMultimedia archivoTemp;
 	public ExploradorRecursivoArchivos(String extensiones) {
+		//iniciar interfaz de transacciones usando 
+		//el patron de disenio Bridge
+		sqlite = new TransaccionesSQLite();
+		biblio = new AlmacenarInfoBibliotecaRefinado(sqlite);
 		StringTokenizer tokens = new StringTokenizer(extensiones, "-");
 		int cont = 0;
 		this.extensiones = new String[tokens.countTokens()+1];
@@ -11,6 +25,11 @@ public class ExploradorRecursivoArchivos {
 			this.extensiones[cont] = tokens.nextToken();
 			cont++;
 		}
+	}
+	public void iniciarExploracion(String ruta){
+		File directorio = new File(ruta);
+		if(directorio.isDirectory() && directorio.exists())
+			explorar(ruta);
 	}
 	public void explorar(String ruta){
 		File directorio = new File(ruta);
@@ -23,9 +42,28 @@ public class ExploradorRecursivoArchivos {
 			}
 			else{
 				for(int j=0;j<extensiones.length-1;j++)
-					if(listado[i].toLowerCase().endsWith(extensiones[j]))
-						System.out.println(listado[i]);
+					if(listado[i].toLowerCase().endsWith(extensiones[j])){
+						if(esVideo(listado[i]))
+							archivoTemp = new Video();
+						archivoTemp.setNombreArchivo(ruta+"/"+listado[i]);
+						archivoTemp.iniciarDatos();
+						biblio.aniadirArchivo(archivoTemp);
+					}
 			}
 		}
+	}
+	private boolean esAudio(String archivo) {
+		for(int i=0;i<extensiones.length;i++){
+			if(archivo.endsWith(".avi"))
+				return true;
+		}
+		return false;
+	}
+	private boolean esVideo(String archivo) {
+		for(int i=0;i<extensiones.length;i++){
+			if(archivo.endsWith(".avi"))
+				return true;
+		}
+		return false;
 	}
 }
